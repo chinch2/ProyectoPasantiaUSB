61f9db0c4f8cbaf7b9517ffb416fd7fab62d221b
Jose Arguello <johao5991@gmail.com>
2019-01-16 23:33:13 -0400

    Primer código de Estación de Entrada
    Primera versión de estación de Entrada.

--- arduino/EstacionEntrada/EstacionEntrada.ino
+++ arduino/EstacionEntrada/EstacionEntrada.ino
@@ -1,52 +1,17 @@
-/*
-  Analog input, analog output, serial output
+// El numero de los pines:
+int button_1 = 13;
+int button_2 = 7;
+int rele = 8;
 
-  Reads an analog input pin, maps the result to a range from 0 to 255 and uses
-  the result to set the pulse width modulation (PWM) of an output pin.
-  Also prints the results to the Serial Monitor.
-
-  The circuit:
-  - potentiometer connected to analog pin 0.
-    Center pin of the potentiometer goes to the analog pin.
-    side pins of the potentiometer go to +5V and ground
-  - LED connected from digital pin 9 to ground
-
-  created 29 Dec. 2008
-  modified 9 Apr 2012
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/AnalogInOutSerial
-*/
-
-// These constants won't change. They're used to give names to the pins used:
-const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
-const int analogOutPin = 9; // Analog output pin that the LED is attached to
-
-int sensorValue = 0;        // value read from the pot
-int outputValue = 0;        // value output to the PWM (analog out)
-
-void setup() {
-  // initialize serial communications at 9600 bps:
-  Serial.begin(9600);
+void setup()
+{
+  pinMode(button_1, INPUT);
+  pinMode(button_2, INPUT);
+  pinMode(rele, OUTPUT);
 }
-
-void loop() {
-  // read the analog in value:
-  sensorValue = analogRead(analogInPin);
-  // map it to the range of the analog out:
-  outputValue = map(sensorValue, 0, 1023, 0, 255);
-  // change the analog out value:
-  analogWrite(analogOutPin, outputValue);
-
-  // print the results to the Serial Monitor:
-  Serial.print("sensor = ");
-  Serial.print(sensorValue);
-  Serial.print("\t output = ");
-  Serial.println(outputValue);
-
-  // wait 2 milliseconds before the next loop for the analog-to-digital
-  // converter to settle after the last reading:
-  delay(2);
+//Las señales provenientes de los pines se leen y se les hace un and
+//De tal manera que si ambos pulsadores estan bajos, se activa el relé
+void loop()
+{
+  digitalWrite(rele, (digitalRead(button_1) && digitalRead(button_2)));
 }
