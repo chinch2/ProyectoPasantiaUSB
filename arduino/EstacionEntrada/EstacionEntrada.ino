c887197e2d90de1547fcf93d16c7cd1d20648f60
Jose Argüello <johao5991@gmail.com>
2019-01-17 12:22:47 -0400

    Modificaciones generales de código

--- arduino/EstacionEntrada/EstacionEntrada.ino
+++ arduino/EstacionEntrada/EstacionEntrada.ino
@@ -17,23 +17,18 @@ void setup()
 }
 //Las señales provenientes de los pines se leen y se les hace un and
 //De tal manera que si ambos pulsadores estan bajos, se activa el relé
-//Inicio con el led apagado
  
-  //----------------MIENTRAS NO TENGO EL RELÉ---------------------
 void loop()
-{//----------------MIENTRAS NO TENGO EL RELÉ---------------------
-  
-  
+{
   //leo el estado de botones que son entradas digitales
   
   boton1 = digitalRead(button_1);
   boton2 = digitalRead(button_2);
-    //----------------MIENTRAS NO TENGO EL RELÉ---------------------
+    
   if (boton1==LOW && boton2==LOW) {
-
+    //ENTRÓ ALGUIEN!
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
     delay(1000);
-     digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off (LOW is the voltage level)
   } 
-  //----------------MIENTRAS NO TENGO EL RELÉ---------------------
 }
