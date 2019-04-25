5a62ad92c110f578395c61f95bf4e2d16b5294d9
Jose Argüello <johao5991@gmail.com>
2019-04-25 17:22:56 -0400

    Pantalla led creado proyecto

---
+++ arduino/Pantallaleds/Pantallaleds.ino
@@ -0,0 +1,12 @@
+int timer = 100;
+int segment[] = {0,1,2,3,4,5,6};
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
